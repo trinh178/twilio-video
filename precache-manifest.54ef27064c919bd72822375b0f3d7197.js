13d262e062c12c0dd3b36a576ada588d7716f62c
Loan Trinh <ltrinh@orangetheoryfitness.com>
2020-04-17 02:58:28 +0700

    Updates

---
+++ precache-manifest.54ef27064c919bd72822375b0f3d7197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3f52e4b20d747f098013fc6f9a3aaa",
+    "url": "/twilio-video/index.html"
+  },
+  {
+    "revision": "fd05a77534d29c60507f",
+    "url": "/twilio-video/static/js/2.47d66fe7.chunk.js"
+  },
+  {
+    "revision": "046558e6751caad2cc7ddb91f4013e3a",
+    "url": "/twilio-video/static/js/2.47d66fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7f0779ea9e8a7a993f",
+    "url": "/twilio-video/static/js/main.70a7da67.chunk.js"
+  },
+  {
+    "revision": "615f3847d74b0fabba0d",
+    "url": "/twilio-video/static/js/runtime-main.8cf76b08.js"
+  },
+  {
+    "revision": "06c50bce4d19c0b71268341f879d7936",
+    "url": "/twilio-video/static/media/google-logo.06c50bce.svg"
+  },
+  {
+    "revision": "62d2e6299a42e4cea2c8c75ac73239d2",
+    "url": "/twilio-video/static/media/twilio-logo.62d2e629.svg"
+  }
+]);
